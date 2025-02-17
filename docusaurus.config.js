401d3265c54c134a73f42079edb8afc945e0bef0
Vstahelin <vitorjosestahelin@gmail.com>
2025-02-17 13:57:50 -0300

    fix: update Docusaurus and React dependencies version

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,8 +1,9 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const {themes} = require('prism-react-renderer');
+const lightTheme = themes.github;
+const darkTheme = themes.dracula;
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -119,7 +120,7 @@ const config = {
         copyright: `Copyright © ${new Date().getFullYear()} whiteStone_dev, Inc. Built with Docusaurus.`,
       },
       prism: {
-        theme: require("prism-react-renderer/themes/vsDark"),
+        additionalLanguages: ['bash', 'diff', 'json'],
       },
     }),
 };
